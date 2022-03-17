dd2cad8b62b5ff5505b0ec89fa35b90810b246ab
linhe <2250167834@qq.com>
2022-03-17 18:11:45 +0800

    linhe

---
+++ exceldata/data/client/FestivalGroupData.lua
@@ -0,0 +1,29 @@
+return {
+    [ 1 ] = {
+        [ "id" ] = 1,
+        [ "ch_key" ] = "欢庆佳节",
+        [ "name" ] = "欢庆佳节",
+        [ "open_time" ] = "2022-03-15 16:00:00",
+        [ "activity_duration" ] = 3,
+        [ "activity_list" ] = {
+            [ 1 ] = 1,
+            [ 2 ] = 2,
+            [ 3 ] = 3,
+            [ 4 ] = 4,
+        },
+        [ "welfare_stuff" ] = 301037,
+        [ "luxury_stuff" ] = 301036,
+        [ "exchange_day" ] = 1,
+        [ "exchange_show_unit" ] = 24041,
+        [ "exchange_tag_list" ] = {
+            [ 1 ] = "豪华兑换",
+            [ 2 ] = "福利兑换",
+        },
+        [ "activity_sell_desc" ] = {
+            [ 1 ] = "节日狂欢",
+            [ 2 ] = "活动兑换",
+        },
+        [ "exchange_bg" ] = 330001,
+        [ "open_timestamp" ] = 1647360000,
+    },
+}
---
+++ exceldata/data/temp_lua_data/DailyRechargeData.lua
@@ -0,0 +1,28 @@
+return {
+    [ [==[__element_names_scheme]==] ] = {
+        [1] = [==[id]==],
+        [2] = [==[activity_time_list]==],
+        [3] = [==[recharge_list]==],
+        [4] = [==[reward_list]==],
+        [5] = [==[recharge_days]==],
+        [6] = [==[luxury_reward]==],
+    },
+    [ [==[__table_field_list]==] ] = {
+    },
+    [ 1 ] = {
+        recharge_list = {1,2,3,4,5,6,7,},
+        recharge_days = 5,
+        luxury_reward = 4015,
+        activity_time_list = {[==[2022-03-10]==],[==[2022-03-17]==],},
+        reward_list = {4001,4002,4003,4004,4005,4006,4007,},
+        id = 1,
+    },
+    [ 2 ] = {
+        recharge_list = {1,2,3,4,5,6,7,},
+        recharge_days = 5,
+        luxury_reward = 4016,
+        activity_time_list = {[==[2022-03-18]==],[==[2022-03-25]==],},
+        reward_list = {4008,4009,4010,4011,4012,4013,4014,},
+        id = 2,
+    },
+}
